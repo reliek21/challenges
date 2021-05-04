2bad831fd8b7fa88f54879a1c3ff56c97c5491d0
Kegubits <KeilerGuardo@Gmail.com>
2021-05-03 19:34:22 -0500

    First challenges from hackerrank

---
+++ hackerrank/01-input-utput.cc
@@ -0,0 +1,14 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n,m,j,result; 
+	cin >> n; cin >> m; cin >> j;
+    result = n+m+j;
+    cout << result << endl;
+    return 0;
+}
---
+++ hackerrank/03-basic-data-types.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int n,n2;
+	char ch;
+	double d,d2;
+	
+	cin >> 
+    return 0;
+}
