d2e0b373d082ea635652c206e8de80bddcafa01f
reliek21 <keilerguardo@gmail.com>
2022-04-18 20:13:23 -0500

    JsChallenger - Comparison operators, strict equality

---
+++ jschallenger/01-basics/02-comparison-operators.js
@@ -0,0 +1,11 @@
+// Write a function that takes two values, say a and b, as arguments
+// Return true if the two values are equal and of the same type
+function myFunction(a, b) {
+    if (a === b) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(myFunction(2, 2));
