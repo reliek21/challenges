7075104b5a337071c01c799bfd764955d83dd94d
reliek21 <keilerguardo@gmail.com>
2022-04-25 14:25:05 -0500

    JSchallenger - Round a number to 2 decimal places

---
+++ jschallenger/01-basics/17-round-number.js
@@ -0,0 +1,16 @@
+// Write a function that takes a number (a) as argument
+// Round a to the 2nd digit after the comma
+// Return the rounded number
+
+function myFunction(a) {
+    return Math.round(a * 100) / 100;
+}
+
+
+// Other way
+// function myFunction(a) {
+    // return Number(a.toFixed(2));
+// }
+
+
+console.log(myFunction(2.12397));
