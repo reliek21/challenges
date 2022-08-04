01765d00dbe392598442cab941ab52b4d54b32e4
reliek21 <keilerguardo@gmail.com>
2022-08-04 08:09:12 -0500

    Clear sort join string

---
+++ jschallenger/01-basics/19-clear-sort-join-strings.js
@@ -0,0 +1,5 @@
+// It seems like something happened to these strings
+// Can you figure out how to clear up the chaos?
+// Write a function that joins these strings together such that they form the following words:
+// 'Javascript', 'Countryside', and 'Downtown'
+// You might want to apply basic JS string methods such as replace(), split(), slice() etc
